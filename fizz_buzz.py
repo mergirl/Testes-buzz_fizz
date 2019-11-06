97547430bd429725c3ca59aa57c8411da0230c13
Nilson Ferreira <nilsonnferreira@icloud.com>
2019-11-06 13:37:00 -0300

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,2 +1,8 @@
 def solucao(n):
-    return
+    if n % 3 == 0:
+        return "fizz"
+    if n % 5 == 0:
+        return "buzz"
+    if n % 3 == 0 and n % 5 == 0:
+        return "buzz-fizz"
+    return n
